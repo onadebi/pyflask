d691c474b458ca0d6d1962f5eeab5018a154c026
Onadebi <onadebi@onadebi.com>
2023-12-12 14:53:51 -0500

    Addition of Service class for handling courses data

---
+++ application/app_services/courses_service.py
@@ -0,0 +1,18 @@
+import json;
+import os;
+
+class CoursesService():
+    def __init__(self):
+        self.courses: list = [];
+        json_file_path = os.path.join(os.getcwd(), 'application','models', 'courses.json');
+        print(f'\n***CWD:: {json_file_path} ***\n')
+        try:
+            with open(json_file_path,'r') as json_file:
+                self.courses = json.load(json_file);
+        except FileNotFoundError:
+            print(f'\n***File not found ERROR:: {json_file_path} ***\n');
+        except Exception as error:
+            print(f'\n***ERROR:: {error} ***\n');
+
+    def get_courses(self) -> list[dict[str,str]]:
+        return self.courses;
--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,11 @@
 from . import app;
 from flask import render_template;
+from .app_services.courses_service import CoursesService as courseSvc;
 
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
-def index():
+def index() -> str:
     return render_template("index.html",title="Home",login_status={'logged_in':False});
 
 
@@ -14,7 +15,9 @@ def login():
 
 @app.route("/courses")
 def courses():
-    return render_template("courses.html",title="Courses");
+    courseData: list = courseSvc().get_courses(); 
+    print(f'\n*** {courseData[3]["title"]} ***\n')
+    return render_template("courses.html",title="Courses",courseData=courseData, courses = True );
 
 @app.route("/register")
 def register():
